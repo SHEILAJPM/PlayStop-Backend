f69c74b7bb651f4f4c99f9d484f34b74dcbe9304
sheilajpm <principemerinosheila@gmail.com>
2026-05-04 23:14:02 -0500

    SE EDITO PARA EL CODIGO DE VERIFICACIÓN PARA QUE TRABAJE JUNTO AL FRONTEND

--- src/main/java/com/playstop/backend/dto/request/ResetPasswordRequest.java
+++ src/main/java/com/playstop/backend/dto/request/ResetPasswordRequest.java
@@ -1,11 +1,17 @@
 package com.playstop.backend.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ResetPasswordRequest {
+
+    // 👇 AQUÍ AGREGAMOS EL CORREO 👇
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "Formato de correo inválido")
+    private String email;
 
     @NotBlank(message = "El código es obligatorio")
     private String code;
